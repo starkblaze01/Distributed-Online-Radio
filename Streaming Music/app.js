965aa5d5b6ff0b80fe398bd2329e3e7317490ec3
Mayank Pathela <mp.pathela@gmail.com>
2019-11-14 23:51:44 +0530

    Add more end-ponits

--- Streaming Music/app.js
+++ Streaming Music/app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.download('./Golden_Time_OP1.mp3'))
+app.get('/stream', (req, res) => res.download('./Golden_Time_OP1.mp3'));
+app.get('/img', (req, res) => res.download('./robot.jpeg'));
+app.get('/name', (req, res) => res.send('Client 1'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 // /Users/starkblaze01/Downloads/ngrok http 3000
